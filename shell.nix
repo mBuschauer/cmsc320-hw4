286da3778aa82e6daffb6b1f6e5d75eac8b63df8
mBuschauer <marco@buschauer.us>
2025-12-09 12:22:35 -0500

    got dependencies working

--- shell.nix
+++ shell.nix
@@ -1,23 +1,35 @@
-{ pkgs ? import <nixpkgs> { } }:
-pkgs.mkShell {
+{ pkgs ? import <nixpkgs> { config = { allowUnfree = true; }; } }:
+let nvidiaPackage = pkgs.linuxPackages.nvidiaPackages.beta;
+in pkgs.mkShell {
   buildInputs = with pkgs.buildPackages; [
     python313
     python313Packages.numpy
     python313Packages.tkinter
     uv
 
-    python313Packages.jupyter
-    python313Packages.ipython
+    procps
+    gnumake
+    util-linux
+    m4
+    gperf
+    stdenv.cc.cc
+    binutils
+    cudatoolkit
+    nvidiaPackage
   ];
 
-  shellHook =
-    ''
-      uv venv --allow-existing
-      uv lock
-      uv sync 
-      source .venv/bin/activate
+  shellHook = ''
+    export CUDA_PATH=${pkgs.cudatoolkit}
+    export EXTRA_LDFLAGS="-L/lib -L${nvidiaPackage}/lib"
+    export EXTRA_CCFLAGS="-I/usr/include"
+    export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib:/run/opengl-driver/lib:$LD_LIBRARY_PATH"
 
-      # jupyter-lab --ip 0.0.0.0
-    '';
+    uv venv --allow-existing
+    uv lock
+    uv sync 
+    source .venv/bin/activate
+
+    # jupyter-lab --ip 0.0.0.0
+  '';
 
 }
